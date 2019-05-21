e7a825278a8c7e906c346bf89a09f208d5204982
Marco Giannini <marco.giannini@6emme.it>
2019-05-21 11:55:35 +0200

    Import Tinson

--- index.php
+++ index.php
@@ -7,24 +7,17 @@
     <!-- Compiled and minified CSS -->
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+    <link rel="stylesheet" href="main.css">
 
     <!-- Compiled and minified JavaScript -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
     <script src="vendor/ajax-snippet.js"></script>
     <title>Tinson</title>
-    <style>
-        .radio-type{
-            margin-bottom: 20px;
-        }
-        .radio-type label{
-            display: block;
-        }
-    </style>
 </head>
 <body>
     <div class="container">
         <div class="row">
-            <form class="col s12" id="add-game">
+            <form class="col s12" id="add-game" onSubmit="tinson.updateGames(event)">
                 <div class="row">
                     <div class="col s12">
                         <h5>Aggiungi un gioco</h5>
@@ -46,7 +39,7 @@
             </form>
         </div>
         <div class="row">
-            <form class="col s12" id="delete-game">
+            <form class="col s12" id="delete-game" onSubmit="tinson.deleteGames(event)">
                 <div class="row">
                     <div class="col s12">
                         <h5>Elimina un gioco</h5>
@@ -64,61 +57,5 @@
         </div>
     </div>
 </body>
-<script>
-    // Add new games
-    let saveForm = document.getElementById('add-game');
-    saveForm.addEventListener('submit', function(event){
-        event.preventDefault();
-        let gid = saveForm.querySelector('#gid');
-        let gname = saveForm.querySelector('#gname');
-
-        ajax.get('switch_games.php', {gid: gid.value, gname: gname.value}, function(response) {
-            M.toast({html: response});
-            gid.value = '';
-            gname.value = '';
-            refreshGames();
-        })
-    })
-
-    let deleteForm = document.getElementById('delete-game');
-    deleteForm.addEventListener('submit', function(event){
-        event.preventDefault();
-        let checkedBoxes = deleteForm.querySelectorAll('input[type="checkbox"]');
-        let toDelete = [];
-        for(let i in checkedBoxes){
-            let box = checkedBoxes[i];
-            if(box.checked){
-                toDelete.push(box.name);
-            }
-        }
-        toDelete = JSON.stringify(toDelete);
-        ajax.get('switch_games.php', {delete: toDelete }, function(response) {
-            M.toast({html: response});
-            refreshGames();
-        })
-    })
-
-    // Get all current games
-    function refreshGames(){
-        const gamesContainer = document.getElementById('games-list');
-        ajax.get('switch_games.php', {list: true}, function(response) {
-            if(response != -1){
-                gamesContainer.innerHTML = '';
-                let gamesList = response.split(';');
-                for(let i in gamesList){
-                    let game = appendGamesInList(gamesList[i]);
-                    gamesContainer.innerHTML += game;
-                }
-            } else {
-                gamesContainer.innerHTML = '<p>Nessun gioco nella lista.</p>';
-            }
-        })
-    }
-    refreshGames();
-
-    function appendGamesInList(gameName){
-        return '<label><input class="filled-in" name="' + gameName + '" type="checkbox"><span>' + gameName + '</span></label>';
-    }
-
-</script>
+<script src="Tinson.js"></script>
 </html>
